a21da62fb5b5aac80e1ec3d6bee3738848fb7d16
Olivier ESUKA <oesukam@gmail.com>
2020-05-12 16:22:02 +0200

    implement first level (choose language)

--- index.php
+++ index.php
@@ -1,33 +1,14 @@
 <?php
   require './connection.php';
+  require './helpers.php';
 
 // The code has completely modified to fit on the need of COVID19-USSD project.
-// Reads the variables sent via POST from our gateway
-  function getParam ($param) {
-    if (isset($_POST[$param])) {
-      return strval($_POST[$param]);
-    }
-    if (isset($_GET[$param])) {
-      return strval($_GET[$param]);
-    }
-    throw new Exception("The index {$param} was undefined", 1);
-  }
-
 
   $db = new Connection();
 
-  echo $db->version() . "\n";
-
- 
-
-  $results = $db->getAllSessions();
-
-  echo "Session ID\tPhone number\n";
-  while ($row = $results->fetchArray())
-  {
-    echo $row['session_id'] . "\t\t" . $row['phone_number'] . "\n";
-  }
-
+  $levels = loadJson('levels');
+  
+  $current_level = 1;
 
   $sessionId   = getParam("sessionId"); // This is a session unique value generated for every session
   $serviceCode =  getParam("serviceCode"); // This is your USSD code.
@@ -39,13 +20,18 @@
 
   if (!$act) {
     $db->insertActivity($sessionId, $phoneNumber, $serviceCode, $text, 1);
+  } else {
+    $current_level = $act['level'];
   }
 
-
-  $response = "\nSession ID: {$sessionId}\n";
-  $response =  $response . "Service code: {$serviceCode}\n";
-  $response = $response . "Phone number: {$phoneNumber}\n";
-  $response = $response . "Input: {$text}";
+  switch ($current_level) {
+    case '1':
+      $response = $levels[$current_level];
+      break;
+    default:
+      $response = $levels['1'];
+      break;
+  }
 
   // Echo the response back to the API
   header('Content-type: text/plain');
